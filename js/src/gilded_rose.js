d945140b3af432071bd13ebf7b5a0fe2118c7179
Zeeshan Aziz <layyah0107@hotmail.com>
2023-03-03 12:57:18 +0500

    set quality for conjured

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -11,7 +11,11 @@ function update_quality() {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+          if (items[i].name.startsWith('Conjured')) {
+            items[i].quality = items[i].quality - 2;
+          } else {
+            items[i].quality = items[i].quality - 1;
+          }
         }
       }
     } else {
@@ -39,17 +43,28 @@ function update_quality() {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
             if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+              if (items[i].name.startsWith('Conjured')) {
+                items[i].quality = items[i].quality - 2;
+              } else {
+                items[i].quality = items[i].quality - 1;
+              }
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = 0;
         }
       } else {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1
         }
       }
+    }
+    
+    if (items[i].quality < 0) {
+      items[i].quality = 0;
+    }
+    if (items[i].quality > 50 && items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      items[i].quality = 50;
     }
   }
 }
